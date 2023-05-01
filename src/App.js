d43dbbb6844dc3eba2aac35bcda1e6043bc9f83b
Narendran G <narensakthiit@gmail.com>
2023-05-01 21:32:29 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,52 +1,20 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import {  useEffect, useContext } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import BookContext from './context/book';
 
 function App() {
-    const [book, setBook] = useState([]);
+    const { fetchBooks } = useContext(BookContext);
 
-    const fetchBooks = async () => {
-        const response = await axios.get('http://localhost:3001/books');
-        setBook(response.data);
-    }
-
-    useEffect(() => { fetchBooks(); }, []);
-
-    const createBook = async (title) => {
-        const respone = await axios.post('http://localhost:3001/books', {
-            title
-        });
-        const updatedBooks = [...book, respone.data];
-        setBook(updatedBooks);
-    }
-
-    const deleteBookById = async (id) => {
-        await axios.delete(`http://localhost:3001/books/${id}`);
-        const updatedBooks = book.filter((book) => {
-            return book.id !== id;
-        });
-        setBook(updatedBooks);
-    }
-
-    const editBookById = async (id, newTitle) => {
-        const response = await axios.put(`http://localhost:3001/books/${id}`,{
-            title: newTitle
-        });
-        const updatedBooks = book.map((book) => {
-            if (book.id === id) {
-                return { ...book, ...response.data }
-            }
-            return book;
-        })
-        setBook(updatedBooks);
-    }
+    useEffect(() => {
+        fetchBooks();
+    }, [fetchBooks]);
 
     return (
         <div className="app">
             <h1>Reading List</h1>
-            <BookList books={book} onDelete={deleteBookById} onEdit={editBookById} />
-            <BookCreate onCreate={createBook} />
+            <BookList />
+            <BookCreate />
         </div>
     );
 }
